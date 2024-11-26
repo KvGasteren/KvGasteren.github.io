ca2dddef60e49845fa4c2c39284a502960dc9acc
Koen van Gasteren <gasteren@gmail.com>
2024-11-26 19:27:54 +0100

    update browserrouter

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { BrowserRouter } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
 	<React.StrictMode>
-		<BrowserRouter basename="/kvgasteren.github.io">
+		<BrowserRouter basename={process.env.NODE_ENV === 'production' ? '/kvgasteren.github.io' : ''}>
 			<App />
 		</BrowserRouter>
 	</React.StrictMode>
