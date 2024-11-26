86c44f79dc2c5fe66eec362032fe12ac5c7359b5
Koen van Gasteren <gasteren@gmail.com>
2024-11-26 18:07:01 +0100

    initial commit

---
+++ src/data/articles.js
@@ -0,0 +1,102 @@
+import React from "react";
+
+function article_1() {
+	return {
+		date: "25 november 2024",
+		title: "Kick Off en Git in Teamverband",
+		description:
+			"De start van het project Quizmaster. Waar we ingedeeld werden in onze teams, een SCRUM game speelden en 's middags worstelden met GIT.",
+		keywords: [
+			"GIT",
+			"Scrum",
+			"Version Control",
+			"QBMK",
+			"Quality Benchmark"
+		],
+		style: `
+				.article-content {
+					display: flex;
+					flex-direction: column;
+					align-items: center;
+				}
+
+				.randImage {
+					align-self: center;
+					outline: 2px solid red;
+				}
+				`,
+		body: (
+			<React.Fragment>
+				<div className="article-content">
+					<div className="paragraph">
+						<h2>Kick Off</h2>
+						<p>
+						We zijn de dag begonnen met de kick-off waarbij de structuur en de gang van zaken van het project werden uitgelegd.
+						Een belangrijke boodschap was dat we geacht werden het zelf uit te zoeken als we er niet uit kwamen.
+						</p>
+					<p>
+						Daarna hadden we een korte teambuilding sessie waabij we een teamnaam kozen, een logo ontwierpen, en een powerpoint presentatie hebben laten genereren door ChatGPT.
+					</p>
+						<p>Het laatste onderdeel van de ochtend was een leuke scrum game. We moesten een productieproces opzetten voor papieren vliegtuigjes. Er was een scoresysteem en we leerden het belang van timeboxing en samenwerking, en ook de valkuilen die horen bij het inschatten van de hoeveelheid werk. Leuk en leerzaam.</p>
+					</div>
+					<img src="../plane.png" alt="paper-plane" width="200"/>
+				</div>
+				<div className="paragraph">
+					<h2>GIT in Teams</h2>
+					<p>
+						Na de lunch zijn we met GIT aan de slag gegaan. We hebben twee workflows bekeken met ons nieuwe teampje. Het pullen, pushen en mergen van en naar de remote repository zorgde voor veel hilariteit en frustratie. Uiteindelijk na veel debuggen zijn we eruit gekomen.
+					</p>
+					<p>
+						We hebben de dag afgesloten met de afspraak morgen in Hilversum samen te werken aan het project.
+					</p>
+				</div>
+			</React.Fragment>
+		),
+	};
+}
+
+function article_2() {
+	return {
+		date: "26 november 2024",
+		title: "Quizmaster Dag 1 - ERD en eerste klassen",
+		description:
+			"Eerste dag - sprintplanning, ontwerp database",
+		style: ``,
+		keywords: [
+			"ERD",
+			"QBMK",
+			"Database",
+			"Sprintplanning",
+		],
+		body: (
+			<React.Fragment>
+				<h1>ERD en sprintplanning spint 1</h1>
+				<div>
+					<h2>ERD</h2>
+					<p>We zijn begonnen met het ontwerpen van de Database. Van 10 tot 11 hebben we een open discussie gevoerd in een van de kamertjes in Hilversum. Aanvankelijk hebben we een draftontwerp gemaakt op het whiteboard. Dat ging vrij soepel. We hebben wel af en toe wat uit moeten vegen.
+						</p>
+						<p>
+							Het tweede deel van de ochtend hebben we de ERD uitgewerkt tot een model dat we met onze PO willen bespreken.
+						</p>
+						<hr/>
+						
+					</div>
+					<div>
+					<h2>De eerste klassen</h2>
+					<p>
+						In de middag zijn we begonnen met een soort sprint planning. Nog niet super gestructureerd - kans voor improvement.
+						We hebben de deliverables van deze week bekeken en ons sprint board gevuld met de tasks en deze verdeeld.
+					</p>
+					<p>
+						Ik heb de basis voor de klasse Vraag gecodeerd en daarna met Bogdan samen in een Code With Me sessie ge-peer-programmed aan het inlezen van de .csv bestanden.
+						Aanvankelijk was ons idee om een INSERT script te genereren, maar het escapen van alle gemene speciale karakters in de wachtwoorden belette ons dat te doen. We hebben voor een oplossing gekozen met gebruik van Prepared Statement.
+					</p>
+					</div>
+			</React.Fragment>
+		),
+	};
+}
+
+const myArticles = [article_1, article_2];
+
+export default myArticles;
---
+++ src/data/user.js
@@ -0,0 +1,84 @@
+const INFO = {
+	main: {
+		title: "MIW Portfolio - SOIA",
+		name: "Koen van Gasteren",
+		email: "gasteren@gmail.com",
+		logo: "../logo.png",
+	},
+
+	socials: {
+		twitter: "https://twitter.com/",
+		github: "https://github.com/",
+		linkedin: "https://linkedin.com/",
+		instagram: "https://instagram.com/",
+		stackoverflow: "https://stackoverflow.com/",
+		facebook: "https://facebook.com/",
+	},
+
+	homepage: {
+		title: "QBMK - Quality Benchmark!",
+		description:
+			"Ik ben een ontwikkelaar-in-spe. Ik heb grote ambitie en neem met Make-IT-Work de teugels in handen om een mooie carriere op te bouwen.",
+	},
+
+	about: {
+		title: "Ik ben Koen van Gasteren en op deze pagina wil ik mijn portfolio tonen.",
+		description:
+			"Voor iedere rubric heb ik een post gemaakt met daarin de gevraagde bewijsmaterialen"
+	},
+
+	articles: {
+		title: "Logboek",
+		description:
+			"Dagelijkse samenvatting van de vorderingen in het project Quizmaster",
+	},
+
+	projects: [
+		{
+			title: "Project 1",
+			description:
+				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			linkText: "View Project",
+			link: "https://github.com",
+		},
+
+		{
+			title: "Project 2",
+			description:
+				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/python/python.png",
+			linkText: "View Project",
+			link: "https://github.com",
+		},
+
+		{
+			title: "Project 3",
+			description:
+				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/html/html.png",
+			linkText: "View Project",
+			link: "https://github.com",
+		},
+
+		{
+			title: "Project 4",
+			description:
+				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			linkText: "View Project",
+			link: "https://github.com",
+		},
+
+		{
+			title: "Project 5",
+			description:
+				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			linkText: "View Project",
+			link: "https://github.com",
+		},
+	],
+};
+
+export default INFO;
