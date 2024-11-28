63237e127e055ce50efec5bccf498af4561785e2
Koen van Gasteren <gasteren@gmail.com>
2024-11-28 11:27:04 +0100

    update homepage.jpg

--- src/pages/homepage.jsx
+++ src/pages/homepage.jsx
@@ -104,7 +104,7 @@ const Homepage = () => {
 								<div className="homepage-image-container">
 									<div className="homepage-image-wrapper">
 										<img
-											src={`${process.env.PUBLIC_URL}/homepage.jpg`}
+											src="homepage.jpg"
 											alt="about"
 											className="homepage-image"
 										/>
